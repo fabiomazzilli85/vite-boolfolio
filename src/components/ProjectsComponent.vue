ef904aa22e5bebb5adccf36c2c798bb53a40f9e7
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-12 18:02:25 +0200

    Creato template. Script ok. Created mi dà problemi: probabile errore di sintassi.

---
+++ src/components/ProjectsComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Projects</h1>
+
+        <ul>
+            <li v-for="project in projects" :key="project.id">
+                {{ project.name }} - {{ project.description }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from axios;
+
+export default {
+    data() {
+        return {
+            projects [],
+        };
+    },
+    created() {
+        this.fetchProjects();
+    },
+}
+</script>
+
+<style></style>
