ddc010e6a3f8da97942c75835862b0f6a9633af5
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-12 20:00:29 +0200

    Bug da ricercare nel path. Codice ok.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <template>
-  <div id="app">
-    <ProjectsComponent />
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App',
-  components: {
-    ProjectsComponent 
-  },
-  data() {
-    return {
-      projects: [] 
-    };
-  },
-  created() {
-    axios.get('http://127.0.0.1:8001/api/projects')
-      .then(response => {
-        this.projects = response.data;
-      })
-      .catch(error => {
-        console.error('Errore nel recuperare i progetti:', error);
-      });
-  }
-};
-</script>
-
-<style>
-</style>
+    <div id="app">
+      <ProjectsComponent />
+    </div>
+  </template>
+  
+  <script>
+  import ProjectsComponent from './components/ProjectsComponent.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      ProjectsComponent
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -7,38 +7,37 @@
                 {{ project.name }} - {{ project.description }}
             </li>
         </ul>
-        
+
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-import ProjectComponent from '@/components/ProjectComponent.vue';
+import ProjectsComponent from './ProjectsComponent.vue';
 
 export default {
-  name: 'App',
-  components: {
-    ProjectComponent
-  },
-  data() {
-    return {
-      projects: [] // Inizializza i dati dei progetti
-    };
-  },
-  created() {
-    this.fetchProjects();
-  },
-  methods: {
-    async fetchProjects() {
-      try {
-        const response = await axios.get('http://127.0.0.1:8001/api/projects');
-        this.projects = response.data;
-      } catch (error) {
-        console.error('Errore nel recuperare i progetti:', error);
-        // Tra l'altro non compare il messaggio in Console Log.
-      }
+    components: {
+        ProjectsComponent
+    },
+    data() {
+        return {
+            projects: [] // Inizializza i dati dei progetti
+        };
+    },
+    created() {
+        this.fetchProjects();
+    },
+    methods: {
+        async fetchProjects() {
+            try {
+                const response = await axios.get('http://127.0.0.1:8001/api/projects');
+                this.projects = response.data;
+            } catch (error) {
+                console.error('Errore nel recuperare i progetti:', error);
+                // Tra l'altro non compare il messaggio in Console Log.
+            }
+        }
     }
-  }
 };
 </script>
 
