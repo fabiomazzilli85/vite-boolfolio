0c95882f926a274f818a9bd07ab54b96b382ae12
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-13 15:06:31 +0200

    Inserito AppContact in pages. Inserito form di contatto con name, email e message.

---
+++ src/pages/AppContact.vue
@@ -0,0 +1,26 @@
+<template>
+<form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Name</label>
+    <input type="text" class="form-control" id="Name" placeholder="What's your name?">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Insert your email address">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Your Message</label>
+    <textarea class="form-control" id="message" name="message" rows="10"></textarea>
+  </div>
+</form>
+
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
