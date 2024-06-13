cc84cdd3763c971764cdfa3de2580b3ccaafdb52
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-13 15:44:26 +0200

    AppPortfolio commentato: mandava in conflitto il sistema, manca AppPortfolio.

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 
 import AppHome from './pages/AppHome.vue'
-import AppPortfolio from './pages/AppPortfolio.vue'
+// import AppPortfolio from './pages/AppPortfolio.vue'
 import AppContact from './pages/AppContact.vue'
 
-const routes = [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/portfolio', name: 'porfolio', component: PortfolioView },
-    { path: '/contact', name: 'contact', component: ContactView },
+const router = [
+    { path: '/', name: 'home', component: AppHome },
+    // { path: '/portfolio', name: 'porfolio', component: PortfolioView },
+    { path: '/contact', name: 'contact', component: AppContact },
   ]
 
-  export default router
+export default router
