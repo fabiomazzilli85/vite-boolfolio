ab573c01cf36b5bf4ba017d89f22b1aff8ae28d2
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-13 14:56:57 +0200

    Creata cartella pages e file AppHome con h1 e p.

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>
+    Ciao!
+</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, tempore!
+</p>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
