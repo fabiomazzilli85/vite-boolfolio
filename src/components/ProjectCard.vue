fa8dc0afe2670404db59edce8f7c5e2904a266bb
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-06-13 14:10:59 +0200

    Script export default e lavorato con props.

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="project-card">
-      <h2>{{ project.name }}</h2>
-      <p>{{ project.description }}</p>
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
     </div>
 </template>
 
+<script>
+export default {
+    name: ProjectsComponent,
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<style></style>
